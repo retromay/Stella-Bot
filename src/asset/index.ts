68599dd57f5804852794ff19698b443af083cbbd
Mayank <mayanknath1997@gmail.com>
2026-03-01 20:59:35 +0530

    fix: fix the image link

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -4,5 +4,5 @@ const ASSET_DIR = path.join(__dirname);
 
 export const assets = {
   bonkExplosion: path.join(ASSET_DIR, "bonk-explosion.gif"),
-  spongebobJail: path.join(ASSET_DIR, "Pilakujail.gif"),
+  spongebobJail: path.join(ASSET_DIR, "Pilakujail.png"),
 } as const;
