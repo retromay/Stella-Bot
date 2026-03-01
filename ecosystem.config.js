beef1ebbd93fee85b1e78d61e2e65dcafdfe7d53
Mayank <mayanknath1997@gmail.com>
2026-03-02 00:15:40 +0530

    fix: deplyment vari

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,30 @@
+const argEnvIndex = process.argv.indexOf("--env");
+let argEnv = (argEnvIndex !== -1 && process.argv[argEnvIndex + 1]) || "";
+
+const RUN_ENV_MAP = {
+  prod: {
+    instances: 1,
+    max_memory_restart: "1000M",
+  },
+};
+
+if (!(argEnv in RUN_ENV_MAP)) {
+  argEnv = "prod";
+}
+
 module.exports = {
   apps: [
     {
       name: "stella-bot",
       script: "dist/index.js",
-      autorestart: true,
-      max_restarts: 10,
-      restart_delay: 5000,
-      env: {
-        NODE_ENV: "production",
+      args: "start",
+      instances: RUN_ENV_MAP[argEnv].instances,
+      exec_mode: "cluster",
+      watch: false,
+      max_memory_restart: RUN_ENV_MAP[argEnv].max_memory_restart,
+      env_prod: {
+        APP_ENV: "prod",
+        PORT: 6350,
       },
     },
   ],
