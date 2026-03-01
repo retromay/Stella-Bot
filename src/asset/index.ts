43ae3efee42ef70fa5d8c86f3153a1cdf34a4414
Mayank <mayanknath1997@gmail.com>
2026-03-01 20:58:45 +0530

    feat: add new picture

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -4,5 +4,5 @@ const ASSET_DIR = path.join(__dirname);
 
 export const assets = {
   bonkExplosion: path.join(ASSET_DIR, "bonk-explosion.gif"),
-  spongebobJail: path.join(ASSET_DIR, "spongebob-jail.gif"),
+  spongebobJail: path.join(ASSET_DIR, "Pilakujail.gif"),
 } as const;
